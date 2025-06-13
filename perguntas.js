2a526cca85682612bbe3e50518141e228fe04d31
HenriqueSouza1112 <henrique.verginelli.souza@escola.pr.gov.br>
2025-06-12 22:50:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Astronomia',
+    'Qual é a estrela do nosso sistema solar?',
+    'Sol'
+)
+
+criaCartao(
+    'Cosmologia',
+    'Qual é o nome da galáxia em que vivemos?',
+    'Via Láctea'
+)
+
+criaCartao(
+    'Sistema Solar',
+    'Qual é o planeta mais próximo da Terra?',
+    'Vênus'
+)
+
+criaCartao(
+    'Ciência Planetária',
+    'Qual planeta foi reclassificado como planeta anão?',
+    'Plutão'
+)
